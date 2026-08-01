09491ccb4ea32cdd44a9caf51bc8d07d9ac82739
Waysys <wshaffer@waysysweb.com>
2026-08-01 19:19:04 -0400

    Update to current month

--- pkg/accounting/calendar_year.go
+++ pkg/accounting/calendar_year.go
@@ -65,11 +65,11 @@ var currentMonth daterange.DateRange
 
 func init() {
 	var err error
-	currentMonthStart, err = d.New(6, 1, 2026)
+	currentMonthStart, err = d.New(7, 1, 2026)
 	if err != nil {
 		panic(err)
 	}
-	currentMonthEnd, err = d.New(6, 30, 2026)
+	currentMonthEnd, err = d.New(7, 31, 2026)
 	if err != nil {
 		panic(err)
 	}
